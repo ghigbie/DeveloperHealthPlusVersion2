7144d2409c7d3f2c2edda0442e06b802c043c240
George Higbie <gehigbie@gmail.com>
2017-01-30 17:28:41 -0700

    working on using changing activities

--- app/src/main/java/com/geogehigbie/developerhealthplusversion2/MainActivity.java
+++ app/src/main/java/com/geogehigbie/developerhealthplusversion2/MainActivity.java
@@ -1,16 +1,14 @@
 package com.geogehigbie.developerhealthplusversion2;
 
-import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import layout.TimeChooserFragment;
 
-    private FragmentManager fragmentManager;
-    private FragmentTransaction fragmentTransaction;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +22,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fragmentManager = getSupportFragmentManager();
-                fragmentTransaction = fragmentManager.beginTransaction();
+                Intent intent = new Intent(getApplicationContext(), TimeChooserFragment.class);
 
 
             }
