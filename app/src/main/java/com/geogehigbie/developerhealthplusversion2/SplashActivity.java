0f7729da7fad647d122a8874693e599aee91d6f8
George Higbie <gehigbie@gmail.com>
2017-01-30 17:06:42 -0700

    added splash activity

--- app/src/main/java/com/geogehigbie/developerhealthplusversion2/SplashActivity.java
+++ app/src/main/java/com/geogehigbie/developerhealthplusversion2/SplashActivity.java
@@ -1,8 +1,21 @@
 package com.geogehigbie.developerhealthplusversion2;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
 /**
  * Created by georgehigbie on 1/30/17.
  */
 
-public class SplashActivity {
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
