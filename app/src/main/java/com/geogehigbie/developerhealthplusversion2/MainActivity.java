f994b44e93fc6b10195947f09dfa9904853e9044
George Higbie <gehigbie@gmail.com>
2017-01-30 18:01:17 -0700

    working on title format

--- app/src/main/java/com/geogehigbie/developerhealthplusversion2/MainActivity.java
+++ app/src/main/java/com/geogehigbie/developerhealthplusversion2/MainActivity.java
@@ -1,19 +1,31 @@
 package com.geogehigbie.developerhealthplusversion2;
 
-import android.content.Intent;
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import layout.TimeChooserFragment;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    android.support.v4.app.FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setOnClickListener();
+    }
+
+    public void centerActionBar(){
+        android.app.ActionBar actionBar = getActionBar();
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); //this is a soft error
+        getSupportActionBar().setCustomView(R.id.actionbar);
     }
 
     public void setOnClickListener(){
@@ -22,8 +34,12 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), TimeChooserFragment.class);
-
+              //  Intent intent = new Intent(getApplicationContext(), TimeChooserFragment.class);
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.fragment_container, new TimeChooserFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
 
             }
         });
