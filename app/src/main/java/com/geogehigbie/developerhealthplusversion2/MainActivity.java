81673303920b4982510397ba58985adbd2e0ec92
George Higbie <gehigbie@gmail.com>
2017-01-30 17:23:13 -0700

    added fragment time chooser

--- app/src/main/java/com/geogehigbie/developerhealthplusversion2/MainActivity.java
+++ app/src/main/java/com/geogehigbie/developerhealthplusversion2/MainActivity.java
@@ -1,13 +1,34 @@
 package com.geogehigbie.developerhealthplusversion2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setOnClickListener(){
+
+        Button button = (Button) findViewById(R.id.improve);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+
+
+            }
+        });
     }
 }
