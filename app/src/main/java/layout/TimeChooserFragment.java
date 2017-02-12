dc120824f56b6959a0da398e6b0773f89dbe5d54
George Higbie <gehigbie@gmail.com>
2017-02-11 22:01:37 -0700

    added variables to timeChooseFragment

--- app/src/main/java/layout/TimeChooserFragment.java
+++ app/src/main/java/layout/TimeChooserFragment.java
@@ -8,8 +8,34 @@ import android.view.ViewGroup;
 
 import com.geogehigbie.developerhealthplusversion2.R;
 
+import java.util.ArrayList;
+
 public class TimeChooserFragment extends Fragment {
+
     private View view;
+    private ArrayList<String> timeArrayListStirng = new ArrayList<String>();
+    private ArrayList<Integer> timeArrayListInt = new ArrayList<Integer>();
+
+    private String pleaseChoose = "Please Select a Time";
+    private String time30String = "30 minutes";
+    private String time45String = "45 minutes";
+    private String time60String = "60 minutes";
+    private String time75String = "1 hour & 15 minutes";
+    private String time90String = "1 hour & 30 minutes";
+    private String time105String = "1 hour & 45 minutes";
+    private String time120String = "2 hours";
+
+    private int time0Int = 0;
+    private int time30Int = 30;
+    private int time45Int = 45;
+    private int time60Int = 60;
+    private int time75Int = 75;
+    private int time105Int = 105;
+    private int time120Int = 120;
+
+    private int NotificationTime;
+
+    private String
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
