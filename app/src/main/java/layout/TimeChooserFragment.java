4da194b62550100d6c80e69b25b110f1cd8016f3
George Higbie <gehigbie@gmail.com>
2017-02-12 18:25:19 -0700

    added items to string array list

--- app/src/main/java/layout/TimeChooserFragment.java
+++ app/src/main/java/layout/TimeChooserFragment.java
@@ -7,7 +7,9 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.geogehigbie.developerhealthplusversion2.R;
 
@@ -16,7 +18,7 @@ import java.util.ArrayList;
 public class TimeChooserFragment extends Fragment {
 
     private View view;
-    private ArrayList<String> timeArrayListStirng = new ArrayList<String>();
+    private ArrayList<String> timeArrayListString = new ArrayList<String>();
     private ArrayList<Integer> timeArrayListInt = new ArrayList<Integer>();
 
     private String pleaseChoose = "Please Select a Time";
@@ -51,6 +53,37 @@ public class TimeChooserFragment extends Fragment {
 
         return view;
     }
+
+    public void setSpinnerContent(){
+
+        ArrayList<String> timeArrayListString = new ArrayList<String>();
+        ArrayList<Integer> timeArrayListInt = new ArrayList<Integer>();
+
+        String pleaseChoose = "Please Select a Time";
+        String time30String = "30 minutes";
+        String time45String = "45 minutes";
+        String time60String = "60 minutes";
+        String time75String = "1 hour & 15 minutes";
+        String time90String = "1 hour & 30 minutes";
+        String time105String = "1 hour & 45 minutes";
+        String time120String = "2 hours";
+
+        timeArrayListStirng.add(pleaseChoose);
+        timeArrayListString.add(time30String);
+
+        int time0Int = 0;
+        int time30Int = 30;
+        int time45Int = 45;
+        int time60Int = 60;
+        int time75Int = 75;
+        int time105Int = 105;
+        int time120Int = 120;
+
+        Spinner spinner1 = (Spinner) view.findViewById(R.id.spinner_time);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_item, timeArrayListStirng);
+
+    }
+
 
     public void addOnClickListener(){
         Button button = (Button) view.findViewById(R.id.time_selected_button);
