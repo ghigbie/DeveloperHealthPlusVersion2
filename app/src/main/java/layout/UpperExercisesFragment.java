4792d1af004737f33634bbbaca940a08bf98c271
George Higbie <gehigbie@gmail.com>
2017-02-28 13:43:11 -0700

    removed misplaced code

--- app/src/main/java/layout/UpperExercisesFragment.java
+++ app/src/main/java/layout/UpperExercisesFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
 import com.geogehigbie.developerhealthplusversion2.R;
 
@@ -25,7 +24,6 @@ public class UpperExercisesFragment extends Fragment {
     }
 
     public void layoutCreation(){
-        RelativeLayout
 
     }
 
